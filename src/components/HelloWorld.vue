d320faaa8bbeb4023c4ec071669a47ff7ca99a42
James.Tsai 蔡承軒 <James.Tsai@Tm-robot.com>
2024-01-12 16:26:17 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ count }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -34,13 +35,22 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    count: {
+      type:[String,Number],
+      default:100
+    }
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello {
+  background-color: fuchsia;
+}
+
 h3 {
   margin: 40px 0 0;
 }
